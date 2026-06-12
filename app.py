21a19f620f184bd80f75c82ce9b6ef5b3b729509
A-Rehman0 <96643540+A-Rehman0@users.noreply.github.com>
2026-06-13 00:06:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,6 +84,7 @@ today = pd.Timestamp.now(tz="Asia/Kolkata").date()
 df['Date'] = df['Date'].dt.tz_localize(None)
 df = df[df['Date'].dt.date <= today].sort_values("Date")
 df['Day'] = df['Date'].dt.strftime('%A')
+df['Date'] = df['Date'].dt.date
 df['Intern Name'] = df['Intern Name'].astype(str).str.strip()
 
 
