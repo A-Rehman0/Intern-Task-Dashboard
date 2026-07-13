665c4ac3a7dbd57b464cc137719f0b13486c4fe2
A-Rehman0 <96643540+A-Rehman0@users.noreply.github.com>
2026-07-13 16:49:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -790,7 +790,7 @@ with tab3:
     c="""{{club_name}}"""
     s="""{{intern_name}}"""
 
-    text4 = f"""<body style="margin:0; padding:0; background-color:#eef1f5; font-family:JetBrains Mono, Helvetica, sans-serif;">
+    text4 = f"""<body style="margin:0; padding:0; background-color:#eef1f5; font-family:JetBrains Mono;">
 
 <!-- Preheader (hidden preview text) -->
 <div style="display:none; max-height:0; overflow:hidden; opacity:0;">
